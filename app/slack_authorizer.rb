306c990596b6757d838e39224533468d274c4e20
“Avit <mohangupta@Mohans-MacBook-Air.local>
2018-08-30 21:28:05 -0700

    Added signed secret validation

---
+++ app/slack_authorizer.rb
@@ -0,0 +1,42 @@
+require 'openssl'
+require "base64"
+
+class SlackAuthorizer
+  ENV['SLACK_SIGNING_SECRET'] = '3f6c063b36ad76e2bd2a1ec80c14af7c'
+  UNAUTHORIZED_MESSAGE = 'Oops! Looks like the application is not authorized! Please review the token configuration.'.freeze
+  UNAUTHORIZED_RESPONSE = ['200', {'Content-Type' => 'text'}, [UNAUTHORIZED_MESSAGE]]
+
+  def initialize(app)
+    @app = app
+  end
+
+  def validate_request?(req)
+    request_body = req.body.read
+    timestamp = req.params['X-Slack-Request-Timestamp']
+    slack_sigining_secret = ENV['SLACK_SIGNING_SECRET']
+    slack_signature = req.params['X-Slack-Signature']
+
+    #if absolute_value(time.time() - timestamp) > 60 * 5:
+    # Too old ignore request
+    if (Time.now.to_i-timestamp.to_i) > 60*5
+        return false
+    else
+        sig_basestring='v0:'<< timestamp << ':' << request_body
+        hash  = OpenSSL::HMAC.digest('sha256', ENV['SLACK_SIGNING_SECRET'], sig_basestring)
+        my_signature = 'v0=' << Base64.encode64(hash)
+	return true if my_signature == slack_signature
+    end
+    false
+   end
+	
+
+  def call(env)
+    req = Rack::Request.new(env)
+
+    if validate_request? req
+      @app.call(env)
+    else
+      UNAUTHORIZED_RESPONSE
+    end
+  end
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require './app'
+require 'rack'
 run Sinatra::Application
