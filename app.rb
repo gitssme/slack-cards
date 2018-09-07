0e46e2b4763b031bf07b011ff5ab96bbc6497eca
“Avit <mohangupta@Mohans-MacBook-Air.local>
2018-09-06 19:48:24 -0700

    Added a test image for the cards

--- app.rb
+++ app.rb
@@ -3,12 +3,13 @@ require_relative 'app/slack_authorizer'
 
 use SlackAuthorizer
 
-HELP_RESPONSE = 'Use `\cards` to display a random card, have fun!'.freeze
+set :public_folder, 'images'
+
+HELP_RESPONSE = 'Use `/cards` to display a random card, have fun!'.freeze
 
 
 get '/card/images/' do
-logger.info "params: #{params.inspect}"
-logger.info "params: #{params}"
+  send_file File.join(settings.public_folder, 'nicubunu-Ornamental-deck-6-of-hearts.svg')
 end
 
 post '/slack/command' do
