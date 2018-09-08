7b3ee1b8d6d7c92f1c5e984c5528c390477006a0
“Avit <mohangupta@Mohans-MacBook-Air.local>
2018-09-07 21:21:43 -0700

    Fixing app file so no more crashing

--- app.rb
+++ app.rb
@@ -8,10 +8,13 @@ set :public_folder, 'images'
 
 HELP_RESPONSE = 'Use `/cards` to display a random card, have fun!'.freeze
 
+OK_RESPONSE = "Thanks for sending this!.".freeze
 
 get '/card/images/*.*' do
   name, ext = params['splat']
   logger.info "#{name} #{ext}" 
+  logger.info "Slack Messenger   #{SlackMessenger.deliver(params['user_name'])}"
+
   send_file File.join(settings.public_folder, "#{name}.#{ ext}" )
 end
 
@@ -19,6 +22,8 @@ post '/slack/command' do
   logger.info "command params: #{params.inspect}"
   case params['text'].to_s.strip
   when 'help' then HELP_RESPONSE 
-  else SlackMessenger.deliver(params['user_name'])
+  else 
+     SlackMessenger.deliver(params['user_name'])
+     OK_RESPONSE
   end
 end
